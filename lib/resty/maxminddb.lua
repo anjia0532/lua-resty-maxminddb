16f5d10261c5ec0a253e6435ad1e1fd3bfd04cc8
AnJia <anjia0532@gmail.com>
2018-07-12 09:22:29 +0800

    fixes #7, fixes #8

--- lib/resty/maxminddb.lua
+++ lib/resty/maxminddb.lua
@@ -16,7 +16,7 @@ local ffi_str             = ffi.string
 local ffi_cast            = ffi.cast
 
 local _M    ={}
-_M._VERSION = '0.05'
+_M._VERSION = '0.06'
 local mt = { __index = _M }
 
 ffi.cdef[[
