9f905e8e42fa61f3a9a7edae1f9c1babef54b0dd
AnJia <anjia0532@gmail.com>
2020-08-06 15:37:25 +0800

    Update maxminddb.lua

--- lib/resty/maxminddb.lua
+++ lib/resty/maxminddb.lua
@@ -1,16 +1,16 @@
--- [[
-Copyright 2017-now anjia (anjia0532@gmail.com)
+--[[
+	Copyright 2017-now anjia (anjia0532@gmail.com)
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-	http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--- ]]
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+]]
 
 -- copy from https://github.com/lilien1010/lua-resty-maxminddb/blob/f96633e2428f8f7bcc1e2a7a28b747b33233a8db/resty/maxminddb.lua#L5-L12
 local json                = require('cjson') 
